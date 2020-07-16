1274052e473c220cf1647c904c9f239e2c9afb78
wcy <s2288156@163.com>
2020-07-16 10:28:47 +0800

    flyway 如果一个数据插入脚本中的某条数据报错，则整个脚本的事务会回滚

---
+++ mall/src/main/resources/db/migration/1.1/V1.1.1__add_one_member.sql
@@ -0,0 +1,3 @@
+insert into db_mall.member (id, create_time, update_time, user_name) value ('10', now(), now(), '老王');
+# insert into db_mall.member (id, create_time, update_time, user_name) value ('11', now(), now(), '老王');
+insert into db_mall.member (id, create_time, update_time, user_name) value ('12', now(), now(), '老王');
