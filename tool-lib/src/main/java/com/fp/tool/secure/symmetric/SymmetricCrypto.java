d5dab54df8d8110058051d28417132616a236e5e
wcy <s2288156@163.com>
2020-07-23 13:40:25 +0800

    SymmetricCrypto 完善功能

--- tool-lib/src/main/java/com/fp/tool/secure/symmetric/SymmetricCrypto.java
+++ tool-lib/src/main/java/com/fp/tool/secure/symmetric/SymmetricCrypto.java
@@ -1,10 +1,12 @@
 package com.fp.tool.secure.symmetric;
 
+import com.fp.tool.secure.Crypto;
+
 import javax.crypto.Cipher;
-import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
-import java.security.NoSuchAlgorithmException;
+import java.nio.charset.StandardCharsets;
 import java.security.spec.AlgorithmParameterSpec;
+import java.util.Base64;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -13,7 +15,7 @@ import java.util.concurrent.locks.ReentrantLock;
  *
  * @author wcy
  */
-public class SymmetricCrypto {
+public class SymmetricCrypto implements Crypto {
 
     private Cipher cipher;
 
@@ -24,12 +26,65 @@ public class SymmetricCrypto {
     private final Lock lock = new ReentrantLock();
 
     public SymmetricCrypto(String algorithm, SecretKey secretKey, AlgorithmParameterSpec parameterSpec) {
+        this(algorithm, secretKey);
+        this.parameterSpec = parameterSpec;
+    }
+
+    public SymmetricCrypto(String algorithm, SecretKey secretKey) {
         try {
             this.cipher = Cipher.getInstance(algorithm);
         } catch (Exception e) {
             throw new IllegalStateException("cipher.getInstance error:", e);
         }
         this.secretKey = secretKey;
-        this.parameterSpec = parameterSpec;
+    }
+
+    @Override
+    public byte[] encrypt(String content) {
+        lock.lock();
+        try {
+            if (this.parameterSpec == null) {
+                cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            } else {
+                cipher.init(Cipher.ENCRYPT_MODE, secretKey, parameterSpec);
+            }
+            return cipher.doFinal(content.getBytes(StandardCharsets.UTF_8));
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public String encryptBase64(String content) {
+        return Base64.getEncoder().encodeToString(encrypt(content));
+    }
+
+    @Override
+    public byte[] decrypt(byte[] encryptContent) {
+        lock.lock();
+        try {
+            if (this.parameterSpec == null) {
+                cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            } else {
+                cipher.init(Cipher.DECRYPT_MODE, secretKey, parameterSpec);
+            }
+            return cipher.doFinal(encryptContent);
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public String decryptStr(byte[] encryptContent) {
+        return new String(decrypt(encryptContent), StandardCharsets.UTF_8);
+    }
+
+    @Override
+    public String decryptStr(String encryptContent) {
+        return decryptStr(Base64.getDecoder().decode(encryptContent));
     }
 }
