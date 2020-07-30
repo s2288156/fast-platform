41ca6098be99f4bb7da40a982ef58d14f5fee4ae
wcy <s2288156@163.com>
2020-07-30 18:52:29 +0800

    User CRUD test用例编写

--- user-dao/src/test/java/com/fp/user/dao/repository/UserRepositoryTest.java
+++ user-dao/src/test/java/com/fp/user/dao/repository/UserRepositoryTest.java
@@ -7,6 +7,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -27,5 +29,34 @@ class UserRepositoryTest extends NoneWebTest {
         assertNotNull(save);
         assertNotNull(save.getId());
         log.info("{}", userDO);
+    }
+
+    @Test
+    void testCRUD() {
+        UserDO userDO = new UserDO();
+        String username = "lao wang";
+        String password = "123456";
+        userDO.setUsername(username);
+        userDO.setPassword(password);
+        userDO.setSex(SexEnum.MAN);
+        UserDO save = userRepository.save(userDO);
+
+        String id = save.getId();
+        assertNotNull(id);
+        String email = "123@1123.com";
+        Optional<UserDO> searchUserOptional = userRepository.findById(id);
+        assertTrue(searchUserOptional.isPresent());
+        UserDO userDOSearch = searchUserOptional.get();
+        assertNull(userDOSearch.getEmail());
+        userDOSearch.setEmail(email);
+
+        userRepository.save(userDOSearch);
+
+        assertEquals(userRepository.findById(id).get().getEmail(), email);
+
+        userRepository.deleteById(id);
+
+        Optional<UserDO> deletedOptional = userRepository.findById(id);
+        assertFalse(deletedOptional.isPresent());
     }
 }
