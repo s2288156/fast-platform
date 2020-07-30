797f3c392238de252c2b73566486fd3112756d07
wcy <s2288156@163.com>
2020-07-30 15:49:31 +0800

    user表创建，以及初始化脚本

---
+++ user-web/src/main/java/com/fp/user/UserApplication.java
@@ -0,0 +1,15 @@
+package com.fp.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wcy
+ */
+@SpringBootApplication
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+}
