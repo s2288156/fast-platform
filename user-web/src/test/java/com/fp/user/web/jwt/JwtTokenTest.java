f36618a62783f0e2bb442d5c6e674f12046dfcae
wcy <s2288156@163.com>
2020-07-31 15:33:59 +0800

    JWS_RSA test case

--- user-web/src/test/java/com/fp/user/web/jwt/JwtTokenTest.java
+++ user-web/src/test/java/com/fp/user/web/jwt/JwtTokenTest.java
@@ -2,11 +2,16 @@ package com.fp.user.web.jwt;
 
 import com.nimbusds.jose.*;
 import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jose.crypto.RSASSASigner;
+import com.nimbusds.jose.crypto.RSASSAVerifier;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.gen.RSAKeyGenerator;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.security.SecureRandom;
+import java.util.Base64;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,11 +30,40 @@ class JwtTokenTest {
         String payloadStr = "hello world";
         byte[] shareKey = new byte[32];
         new SecureRandom().nextBytes(shareKey);
-        log.warn("shareKey = {}", new String(shareKey));
+        log.warn("shareKey = {}", Base64.getEncoder().encodeToString(shareKey));
+        log.warn("shareKey = {}", Base64.getEncoder().encodeToString(shareKey));
+        log.warn("shareKey = {}", Base64.getEncoder().encodeToString(shareKey));
         Payload payload = new Payload(payloadStr);
         JWSObject jwsObject = new JWSObject(jwsHeader, payload);
         JWSSigner jwsSigner = new MACSigner(shareKey);
         jwsObject.sign(jwsSigner);
         log.warn("{}", jwsObject.serialize());
+    }
+
+    @SneakyThrows
+    @Test
+    void testJWS_RSA() {
+        RSAKey rsaJWK = new RSAKeyGenerator(2048).keyID("123").generate();
+        RSAKey rsaPublicJWK = rsaJWK.toPublicJWK();
+
+        RSASSASigner signer = new RSASSASigner(rsaJWK);
+
+        String content = "In RSA we trust!";
+        JWSObject jwsObject = new JWSObject(
+                new JWSHeader.Builder(JWSAlgorithm.RS256).keyID(rsaJWK.getKeyID()).build(),
+                new Payload(content)
+        );
+
+        jwsObject.sign(signer);
+
+        String s = jwsObject.serialize();
+        log.warn("serialize = {}", s);
+        assertNotNull(s);
+
+        JWSObject parseJwsObject = JWSObject.parse(s);
+
+        RSASSAVerifier verifier = new RSASSAVerifier(rsaPublicJWK);
+        assertTrue(parseJwsObject.verify(verifier));
+        assertEquals(content, parseJwsObject.getPayload().toString());
     }
 }
