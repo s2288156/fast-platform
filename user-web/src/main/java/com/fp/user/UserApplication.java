145e209d4b3d0893749c8916c85fe5e7acb10698
wcy <s2288156@163.com>
2020-08-12 15:53:49 +0800

    user-web 集成nacos-discovery

--- user-web/src/main/java/com/fp/user/UserApplication.java
+++ user-web/src/main/java/com/fp/user/UserApplication.java
@@ -2,10 +2,12 @@ package com.fp.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author wcy
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class UserApplication {
 
