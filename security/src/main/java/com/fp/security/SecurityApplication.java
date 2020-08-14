69b88d7eab81d7042318353f350426b03c654dd2
wcy <s2288156@163.com>
2020-08-14 16:09:31 +0800

    security module 创建，测试

---
+++ security/src/main/java/com/fp/security/SecurityApplication.java
@@ -0,0 +1,14 @@
+package com.fp.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wcy
+ */
+@SpringBootApplication
+public class SecurityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
+}
---
+++ security/src/main/java/com/fp/security/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.fp.security;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author wcy
+ */
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+}
