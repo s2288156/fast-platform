e05b3774301766eb780ef1a077341d453db82e94
wcy <s2288156@163.com>
2020-08-17 16:24:24 +0800

    SecurityConfig 配置login成功后重定向到/user/all接口

--- user-web/src/main/java/com/fp/user/web/config/SecurityConfig.java
+++ user-web/src/main/java/com/fp/user/web/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.fp.user.web.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
 /**
  * @author wcy
@@ -24,17 +26,38 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .headers().frameOptions().disable()
-                .and();
-
+        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+        successHandler.setTargetUrlParameter("redirectTo");
+        successHandler.setDefaultTargetUrl("/all");
         http
                 .authorizeRequests()
-                .antMatchers("/**").permitAll()
-                .and();
+                .antMatchers("/login", "/all").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login")
+                .defaultSuccessUrl("/all")
+                .successHandler(successHandler)
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable();
+//        http
+//                .csrf().disable()
+//                .headers().frameOptions().disable()
+//                .and();
+//
+//        http
+//                .authorizeRequests()
+//                .antMatchers("/**").permitAll()
+//                .and();
     }
 
     @Override
