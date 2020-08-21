bd930bf7e871867955af045a1c0fc92f4145a3b8
wcy <s2288156@163.com>
2020-08-21 18:00:56 +0800

    feat(user-web): mapper scan警告消除

--- user-dao/src/main/java/com/fp/user/dao/config/MyBatisPlusConfig.java
+++ user-dao/src/main/java/com/fp/user/dao/config/MyBatisPlusConfig.java
@@ -1,15 +1,12 @@
 package com.fp.user.dao.config;
 
-import com.baomidou.mybatisplus.core.config.GlobalConfig;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * @author wcy
  */
-@MapperScan("com.fp.user.dao.mapper")
 @Configuration
 public class MyBatisPlusConfig {
 
--- user-web/src/main/java/com/fp/UserApplication.java
+++ user-web/src/main/java/com/fp/UserApplication.java
@@ -1,5 +1,6 @@
 package com.fp;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,6 +9,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 /**
  * @author wcy
  */
+@MapperScan("com.fp.user.dao.mapper")
 @EnableTransactionManagement
 @EnableDiscoveryClient
 @SpringBootApplication
