9a55029097137b2a287ab45443aff9840e78a808
wcy <s2288156@163.com>
2020-09-18 18:18:13 +0800

    feat(gateway): 接入oauth2

---
+++ gateway/src/main/java/com/fp/gateway/security/ResourceServerConfig.java
@@ -0,0 +1,23 @@
+package com.fp.gateway.security;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author wcy
+ */
+@AllArgsConstructor
+@Configuration
+@EnableWebFluxSecurity
+public class ResourceServerConfig {
+
+
+
+}
