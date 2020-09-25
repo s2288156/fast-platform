deb79db1dd5328fe641ad354b9914a2d57a1bd57
wcy <s2288156@163.com>
2020-09-25 17:44:49 +0800

    feat(gateway): 接入oauth2

---
+++ gateway/src/main/java/com/fp/gateway/security/IgnoreUrlsConfig.java
@@ -0,0 +1,19 @@
+package com.fp.gateway.security;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * @author wcy
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Component
+@ConfigurationProperties(prefix="secure.ignore")
+public class IgnoreUrlsConfig {
+    private List<String> urls;
+}
