5b7a516c4a10b835544ad9cd5bec9abbd0b104c9
wcy <s2288156@163.com>
2020-10-10 17:36:54 +0800

    oauth/token接口实现

---
+++ demo-auth/src/main/java/com/fp/auth/controller/AuthController.java
@@ -0,0 +1,41 @@
+package com.fp.auth.controller;
+
+
+import com.fp.auth.constant.AuthConstant;
+import com.fp.auth.domain.CommonResult;
+import com.fp.auth.domain.Oauth2TokenDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.endpoint.TokenEndpoint;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.Map;
+
+/**
+ * 自定义Oauth2获取令牌接口
+ * Created by macro on 2020/7/17.
+ */
+@RestController
+@RequestMapping("/oauth")
+public class AuthController {
+
+    @Autowired
+    private TokenEndpoint tokenEndpoint;
+
+    @RequestMapping(value = "/token", method = RequestMethod.POST)
+    public CommonResult<Oauth2TokenDto> postAccessToken(Principal principal, @RequestParam Map<String, String> parameters) throws HttpRequestMethodNotSupportedException {
+        OAuth2AccessToken oAuth2AccessToken = tokenEndpoint.postAccessToken(principal, parameters).getBody();
+        Oauth2TokenDto oauth2TokenDto = Oauth2TokenDto.builder()
+                .token(oAuth2AccessToken.getValue())
+                .refreshToken(oAuth2AccessToken.getRefreshToken().getValue())
+                .expiresIn(oAuth2AccessToken.getExpiresIn())
+                .tokenHead(AuthConstant.JWT_TOKEN_PREFIX).build();
+
+        return CommonResult.success(oauth2TokenDto);
+    }
+}
